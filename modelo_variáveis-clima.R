aacc42c31721a9a2d03c9752e18391eb36551bba
LiviaDias <diascclivia@gmail.com>
2020-12-28 21:56:20 -0300

    atualização script do modelo

--- modelo_variáveis-clima.R
+++ modelo_variáveis-clima.R
@@ -1,6 +1,6 @@
 #teste regressão logística com variáveis climáticas e focos de incêndios
 
-dados_cavernas= data.frame(read.csv("cavernas_model-clima.csv", sep = ","))
+
 
 
 #gera intervalo de valores a partir de min e max de uma coluna, incrementando pelo valor de "interval"
@@ -45,42 +45,80 @@ create_data = function(dataframe) {
   
   data_tibble = as_tibble(dataframe)
   
-  #arrumando valores e fatores
-  data_tibble$precipitation[data_tibble$precipitation<0]=0
-  data_tibble$HAS_FIRE=as.factor(data_tibble$HAS_FIRE)
-  data_tibble$Focuses_qnt=as.numeric(data_tibble$Focuses_qnt)
-  data_tibble$HAS_FIRE=relevel(data_tibble$HAS_FIRE, "false")
-  
-  #criando index de treino e dados de treino e teste
-  index_treino=sample(1:nrow(data_tibble), round(0.8*nrow(data_tibble)))
-  dados_treino=data_tibble[index_treino,]
-  dados_teste=data_tibble[-index_treino,]
-  
-
-  Queimou=dados_treino[dados_treino$HAS_FIRE=="true",]
-  Não_queimou=dados_treino[dados_treino$HAS_FIRE=="false",]
-
   #gerando categorias de cada variável:
   #temperatura: incrementando de 2 em 2 graus de min até o max da coluna
   #rh: gerando 10 intervalos iguais entre o min e o max
   #velocidade do vento: incrementando de 2 em 2 m/s de min até o max da coluna
-  #precipitação: incrementando de 2 em 2 milimetros de min até o max da coluna
+  #precipitação: incrementando de 10 em 10 milimetros de min até o max da coluna
   setDT(data_tibble)
   temp_cat = cut(data_tibble$temperature,
-                 get_interval(data_tibble$temperature, 2),
+                 get_interval_from_quantity(data_tibble$temperature, 10),
                  include.lowest=TRUE)
   rh_cat = cut(data_tibble$Relative.Humidity,
-               get_interval(data_tibble$Relative.Humidity, 10),
+               get_interval_from_quantity(data_tibble$Relative.Humidity, 10),
                include.lowest=TRUE)
   ws_cat = cut(data_tibble$Wind.speed,
-               get_interval(data_tibble$Wind.speed, 2),
+               get_interval_from_quantity(data_tibble$Wind.speed, 10),
                include.lowest=TRUE)
   prec_cat = cut(data_tibble$precipitation,
-                 get_interval(data_tibble$precipitation, 2),
+                 get_interval_from_quantity(data_tibble$precipitation, 10),
                  include.lowest=TRUE)
   dados_out = data.frame(data_tibble, temp_cat,rh_cat,ws_cat,prec_cat)
   
   return(dados_out)
 }
 
-out = create_data(dados_cavernas)
+dados_cavernas= data.frame(read.csv("cavernas_model-clima.csv", sep = ","))
+cavernas_categorizado= create_data(dados_cavernas)
+
+dados_pandeiros= data.frame(read.csv("pandeiros_model-clima.csv", sep = ","))
+pandeiros_categorizado= create_data(dados_pandeiros)
+
+dados_cocha= data.frame(read.csv("cocha_model-clima.csv", sep = ","))
+cocha_categorizado= create_data(dados_cocha)
+
+#arrumando valores e fatores
+
+
+tibble_variaveis= function(apa_categorizada){
+  data_tibble = as_tibble(apa_categorizada) 
+  data_tibble$HAS_FIRE=as.factor(data_tibble$HAS_FIRE)
+  data_tibble$Focuses_qnt=as.numeric(data_tibble$Focuses_qnt)
+  data_tibble$HAS_FIRE=relevel(data_tibble$HAS_FIRE, "false")
+  
+  return(data_tibble)
+}
+
+#apa cavernas - amostras
+tibble_cavernas=tibble_variaveis(cavernas_categorizado)
+
+
+#criando index de treino e dados de treino e teste
+index_treino_cavernas=sample(1:nrow(tibble_cavernas), round(0.8*nrow(tibble_cavernas)))
+dados_treino_cavernas=tibble_cavernas[index_treino_cavernas,]
+dados_teste_cavernas=tibble_cavernas[-index_treino_cavernas,]
+
+
+Queimou=dados_treino_cavernas[dados_treino_cavernas$HAS_FIRE=="true",]
+Não_queimou=dados_treino_cavernas[dados_treino_cavernas$HAS_FIRE=="false",]
+
+#apa cocha - amostras
+tibble_cocha=tibble_variaveis(cocha_categorizado)
+
+index_treino_cocha=sample(1:nrow(tibble_cocha), round(0.8*nrow(tibble_cocha)))
+dados_treino_cocha=tibble_cocha[index_treino_cocha,]
+dados_teste_cocha=tibble_cocha[-index_treino_cocha,]
+
+Queimou=index_treino_cocha[index_treino_cocha$HAS_FIRE=="true",]
+Não_queimou=index_treino_cocha[index_treino_cocha$HAS_FIRE=="false",]
+
+#apa pandeiros - amostras
+tibble_pandeiros=tibble_variaveis(pandeiros_categorizado)
+
+index_treino_pandeiros=sample(1:nrow(tibble_pandeiros), round(0.8*nrow(tibble_pandeiros)))
+dados_treino_pandeiros=tibble_pandeiros[index_treino_pandeiros,]
+dados_teste_pandeiros=tibble_pandeiros[-index_treino_pandeiros,]
+
+
+Queimou=dados_treino_pandeiros[dados_treino_pandeiros$HAS_FIRE=="true",]
+Não_queimou=dados_treino_pandeiros[dados_treino_pandeiros$HAS_FIRE=="false",]
