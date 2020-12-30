735292e84b62d2f00e2eecd95af03a3a6dec4829
LiviaDias <diascclivia@gmail.com>
2020-12-30 17:54:03 -0300

    rodando o modelo para as apas e atualizações

--- modelo_variáveis-clima.R
+++ modelo_variáveis-clima.R
@@ -84,7 +84,8 @@ tibble_variaveis= function(apa_categorizada){
   data_tibble = as_tibble(apa_categorizada) 
   data_tibble$HAS_FIRE=as.factor(data_tibble$HAS_FIRE)
   data_tibble$Focuses_qnt=as.numeric(data_tibble$Focuses_qnt)
-  data_tibble$HAS_FIRE=relevel(data_tibble$HAS_FIRE, "false")
+  levels(data_tibble$HAS_FIRE)=c("not-burned","burned") 
+  data_tibble$HAS_FIRE=relevel(data_tibble$HAS_FIRE, "not-burned")
   
   return(data_tibble)
 }
@@ -92,8 +93,7 @@ tibble_variaveis= function(apa_categorizada){
 #apa cavernas - amostras
 tibble_cavernas=tibble_variaveis(cavernas_categorizado)
 
-#categorias de referência
-tibble_cavernas$HAS_FIRE=relevel(tibble_cavernas$HAS_FIRE,"false")
+
 
 #criando index de treino e dados de treino e teste
 index_treino_cavernas=sample(1:nrow(tibble_cavernas), round(0.8*nrow(tibble_cavernas)))
@@ -104,8 +104,6 @@ table(dados_treino_cavernas$HAS_FIRE,dados_treino_cavernas$rh_cat)
 table(dados_treino_cavernas$HAS_FIRE,dados_treino_cavernas$temp_cat)
 table(dados_treino_cavernas$HAS_FIRE,dados_treino_cavernas$ws_cat)
 table(dados_treino_cavernas$HAS_FIRE,dados_treino_cavernas$prec_cat)
-
-barplot(table(dados_treino_cavernas$HAS_FIRE,dados_treino_cavernas$temp_cat),main = "temperatura x queimadas")
 
 
 #apa cocha - amostras
@@ -142,20 +140,46 @@ model2=glm(HAS_FIRE~temperature+Relative.Humidity+precipitation+Wind.speed,data 
 summary(model2)
 
 #modelo sem precipitação
-model3=glm(HAS_FIRE~temperature+Relative.Humidity+Wind.speed,data = dados_treino_cavernas,family = binomial())
+model_cavernas=glm(HAS_FIRE~temperature+Relative.Humidity+Wind.speed,data = dados_treino_cavernas,family = binomial())
 
-summary(model3)
+summary(model_cavernas)
 
-#variáveis numéricas amostra de teste - rodando o modelo
+#variáveis numéricas amostra de teste-cavernas - rodando o modelo
 
-pred.Teste=predict(model3,dados_teste_cavernas,type = "response")
+pred.Teste=predict(model_cavernas,dados_teste_cavernas,type = "response")
 View(pred.Teste)
 
 dados_teste_cavernas$Prob_fogo=pred.Teste
 
-dados_teste_cavernas[,c("HAS_FIRE","Prob_fogo")]
+View(dados_teste_cavernas[,c("HAS_FIRE","Prob_fogo")])
+
+#rodando modelo para apa cocha
+model_cocha=glm(HAS_FIRE~-1+temperature+Relative.Humidity+Wind.speed,data = dados_treino_cocha,family = binomial())
+
+summary(model_cocha)
+
+pred.Cocha=predict(model_cocha,dados_teste_cocha,type = "response")
+View(pred.Cocha)
+
+dados_teste_cocha$Prob_fogo=pred.Cocha
+
+View(dados_teste_cocha[,c("HAS_FIRE","Prob_fogo")])
+
+table(dados_teste_cocha$HAS_FIRE,dados_teste_cocha$Prob_fogo>0.5)
+
+#rodando modelo para apa pandeiros
+model_pandeiros=glm(HAS_FIRE~-1+temperature+Relative.Humidity+Wind.speed,data = dados_treino_pandeiros,family = binomial())
+
+summary(model_pandeiros)
+
+pred.Pandeiros=predict(model_pandeiros,dados_teste_pandeiros,type = "response")
+View(pred.Pandeiros)
+
+dados_teste_pandeiros$Prob_fogo=pred.Pandeiros
+
+View(dados_teste_pandeiros[,c("HAS_FIRE","Prob_fogo")])
+
+table(dados_teste_pandeiros$HAS_FIRE,dados_teste_pandeiros$Prob_fogo>0.5)
 
 
 
-cat("taxa de acerto de queima",53,"%\n")
-cat("taxa de acerto de não_queima",90,"%\n")
