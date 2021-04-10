05b545585aa5e2dbc04dcd8c4acd91163444c6df
LiviaDias <diascclivia@gmail.com>
2021-04-10 13:36:03 -0300

    Organizando

--- lasso bom.R
+++ lasso bom.R
@@ -1,7 +1,14 @@
 #load required library
 library(glmnet)
+library(coefplot)
 
-cavernas_topografic=data.frame(read.csv("modelo_topografico_cavernas.csv",dec = ",", sep = ";"))
+titulo_grafico_pred = "Lasso Regression - APACP"
+arquivo_apa_leitura = "modelo_topográfico\\APA Cavernas\\matrix_topografic_cave.csv"
+arquivo_coef_saida = "modelo_topográfico\\APA Cavernas\\resultados\\coeficientes_lasso_cave.csv"
+arquivo_prob_saida = "modelo_topográfico\\APA Cavernas\\resultados\\probabilidade_lasso_cave.csv"
+
+
+cavernas_topografic=data.frame(read.csv(arquivo_apa_leitura,dec = ",", sep = ";"))
 
 treino_cave_lasso=sample(1:nrow(cave_map_lasso), round(0.8*nrow(cave_map_lasso)))
 dados_treino_cave_lasso=cave_map_lasso[treino_cave_lasso,]
@@ -14,16 +21,21 @@ y <- ifelse(dados_treino_cave_lasso$HAS_FIRE==1,1,0)
 #perform grid search to find optimal value of lambda
 #family= binomial => logistic regression, alpha=1 => lasso
 # check docs to explore other type.measure options
-cv.out <- cv.glmnet(x,y,alpha=1)
+cv.out <- cv.glmnet(x,y,alpha=1,family="binomial")
 #plot result
 plot(cv.out)
+
 
 #min value of lambda
 lambda_min <- cv.out$lambda.min
 #best value of lambda
 lambda_1se <- cv.out$lambda.1se
 #regression coefficients
-coef(cv.out,s=lambda_1se)
+coeficients=coef(cv.out,s=lambda_1se)
+coefplot(cv.out, lambda=lambda_1se, sort="magnitude")
+coefpath(cv.out)
+
+write.csv(as.data.frame(as.matrix(coeficients)),file = arquivo_coef_saida)
 
 #get test data
 x_test <- model.matrix(HAS_FIRE~.,dados_teste_cave_lasso)
@@ -32,7 +44,30 @@ lasso_prob <- predict(cv.out,newx = x_test,s=lambda_1se,type="response")
 #translate probabilities to predictions
 lasso_predict <- rep(0,nrow(dados_teste_cave_lasso))
 lasso_predict[lasso_prob>.5] <- 1
+
+
+results_cave_lasso=cbind(lasso_predict,lasso_prob)
+colnames(results_cave_lasso)=c("lasso_predict","lasso_prob")
+rownames(results_cave_lasso)=c()
+write.csv(results_cave_lasso,file = arquivo_prob_saida,row.names = FALSE)
 #confusion matrix
 table(pred=lasso_predict,true=dados_teste_cave_lasso$HAS_FIRE)
 #accuracy
 mean(lasso_predict==dados_teste_cave_lasso$HAS_FIRE)
+
+#grafico
+data_cave=cbind.data.frame(dados_teste_cave_lasso$HAS_FIRE,lasso_prob,lasso_predict)
+names(data_cave)=c("HAS_FIRE","lasso_prob","lasso_predict")
+View(data_cave)
+
+grafico_predicao <-ggplot(data_cave, aes(x=lasso_prob, y=lasso_predict)) + geom_point() + 
+  geom_smooth(method="glm", family="binomial", col="red") + xlab("Probability") + 
+  ylab("Predicted") + labs(title = titulo_grafico_pred) 
+  #+ labs(tag="a)")
+grafico_predicao
+
+grafico_has_fire <-ggplot(data_cave, aes(x=lasso_prob, y=dados_teste_cave_lasso$HAS_FIRE)) + geom_point() + 
+  geom_smooth(method="glm", family="binomial", col="red") + xlab("Probability") + 
+  ylab("Predicted") + labs(title = "True Occurrences") 
+#+ labs(tag="a)")
+grafico_has_fire
